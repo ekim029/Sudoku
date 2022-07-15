5f4e7e50fd66a09be2c28e023bb00920cbfbe479
Hyun <ekim029@bu.edu>
2022-07-15 10:03:50 -0400

    class Sudoku

--- Sudoku.py
+++ Sudoku.py
@@ -1,1 +1,23 @@
 
+class Sudoku:
+
+    def main():
+        puzzle = Puzzle()
+        fileName = input("Please enter the name of puzzle file: ")
+
+        try:
+            inputFile = open(fileName, 'r')
+            puzzle.readFrom(inputFile)
+        except:
+            print("Error accessing file " + fileName)
+        
+        print("Here is the inial puzzle: ")
+        puzzle.display()
+
+        if puzzle.solve():
+            print("Here is the solution: ")
+        else:
+            print("No solution could be found")
+            print("Here is the current state of the puzzle")
+        
+        puzzle.display()
